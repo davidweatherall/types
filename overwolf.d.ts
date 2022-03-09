fba0faa9c5557e4cecbad44ecf63d6d1fa0b8b05
shai.abraham <shai.abraham@overwolf.com>
2022-03-09 19:57:32 +0200

    added `overwolf.windows.setBounds`

--- overwolf.d.ts
+++ overwolf.d.ts
@@ -1490,6 +1490,12 @@ declare namespace overwolf.windows {
     auto_dpi_resize?: boolean;
   }
 
+  interface SetWindowBoundsParams {
+    window_id: string;
+    bounds: ODKRect;
+    auto_dpi_resize?: boolean;
+  }
+
   interface WindowStateChangedEvent {
     window_id: string;
     window_state: string;
@@ -1650,6 +1656,16 @@ declare namespace overwolf.windows {
    */
   function changeSize(
     changeSizeParams: ChangeWindowSizeParams,
+    callback?: CallbackFunction<Result>
+  ): void;
+
+  /**
+   * Set window position and window size.
+   * @param boundsParams 
+   * @param callback 
+   */
+  function setBounds(
+    boundsParams: SetWindowBoundsParams,
     callback?: CallbackFunction<Result>
   ): void;
 
